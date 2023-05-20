a756be142b9dd885eb99ff9fc6b8f19047c0cc5b
TalentDev1128 <razorking1128@gmail.com>
2023-05-20 07:03:23 -0400

    update code

--- app/controllers/trigger_controller.rb
+++ app/controllers/trigger_controller.rb
@@ -1,19 +1,12 @@
 require "#{Rails.root}/app/workers/redis_event_worker.rb"
 class TriggerController < ApplicationController
   def index
-    render plain: Time.now.to_s
-    send_event_to_redis
+    Redis.new.lpush('trigger_queue', Time.now.to_s)
+    render plain: "Event added to queue at #{Time.now}"
+    
   end
 
-  private
-
-  def send_event_to_redis
-    current_time = Time.now
-    last_request_time = Datetime.last&.last_request_time
-
-    if last_request_time.nil? || current_time - last_request_time > 60
-      RedisEventWorker.perform_async
-      Datetime.create(last_request_time: current_time)
-    end
+  def startRedisWorker
+    RedisEventWorker.perform_async
   end
 end
--- app/workers/redis_event_worker.rb
+++ app/workers/redis_event_worker.rb
@@ -5,9 +5,17 @@ class RedisEventWorker
     last_request_time = Datetime.last&.last_request_time
     return unless last_request_time
 
-    current_time = Time.now
-    return if current_time - last_request_time < 60
+    while next_event = Redis.new.rpop('trigger_queue')
+      if Time.parse(Time.now.to_s) - Time.parse(next_event) >= 60
+        Redis.new.publish('events', next_event.to_s)
+        last_request_time = Time.parse(next_event)
+        Datetime.create(last_request_time: last_request_time)
+      else
+        Redis.new.lpush('trigger_queue', next_event.to_s)
+        break
+      end
+    end
 
-    Redis.new.publish('events', last_request_time.to_s)
+    self.class.perform_in(3.seconds)
   end
 end
